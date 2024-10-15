b4b563cdd821bec4bfd83ac062c0868f4fc71fb0
yuhyejin <hj_eu@naver.com>
2024-10-15 12:30:49 +0900

    commit: health 수정

--- src/main/java/com/myhandjava/jenkinsproject/controller/CalculatorController.java
+++ src/main/java/com/myhandjava/jenkinsproject/controller/CalculatorController.java
@@ -21,7 +21,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!!";
+        return "I'm alive!!!@@@";
     }
 
     @GetMapping("/plus")
